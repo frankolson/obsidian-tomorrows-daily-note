4002c8d044e74eb7ef5af471bc0af43c00b19cb2
Will Olson <will.olson11@gmail.com>
2021-12-28 15:45:52 -0500

    Create new daily note with command palatte command

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,35 @@
 import { Plugin, Notice } from "obsidian";
+import {
+  appHasDailyNotesPluginLoaded,
+  createDailyNote
+} from "obsidian-daily-notes-interface";
 
 export default class TomorrowsDailyNote extends Plugin {
 
+  createTomorrowsDailyNote() {
+    if (appHasDailyNotesPluginLoaded()) {
+      const { moment } = window
+      const tomorrow = moment().add(1, 'days')
+
+      createDailyNote(tomorrow)
+    } else {
+      new Notice("The Daily Notes plugin needs to be enabled for this command to work")
+    }
+  }
+
   onload() {
-    console.log("Loading plugin: Tomorrow's Daily Note");
+    console.log("Loading plugin: Tomorrow's Daily Note")
 
     this.addCommand({
       id: 'create-tomorrows-daily-note',
       name: 'Create tomorrow\'s daily note',
       callback: () => {
-        new Notice('Test for creating the next daily note...')
+        this.createTomorrowsDailyNote()
       }
-    });
+    })
   }
 
 	onunload() {
-    console.log("Unloading plugin: Tomorrow's Daily Note");
+    console.log("Unloading plugin: Tomorrow's Daily Note")
   }
 }
