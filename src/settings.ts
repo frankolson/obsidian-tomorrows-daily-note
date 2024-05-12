7656c687991f2fec8356242299ae0598dd4d97ad
Will Olson <will.olson11@gmail.com>
2024-05-12 11:54:04 -0400

    Add  option to plugin settings

--- src/settings.ts
+++ src/settings.ts
@@ -3,10 +3,12 @@ import { App, PluginSettingTab, Setting } from "obsidian";
 
 export interface TomorrowsDailyNoteSettings {
   skipWeekends: boolean;
+  enableRibbonIcon: boolean;
 }
 
 export const DEFAULT_SETTINGS: Partial<TomorrowsDailyNoteSettings> = {
-  skipWeekends: false
+  skipWeekends: false,
+  enableRibbonIcon: true,
 };
 
 export class TomorrowsDailyNoteSettingTab extends PluginSettingTab {
@@ -31,6 +33,24 @@ export class TomorrowsDailyNoteSettingTab extends PluginSettingTab {
           .onChange(async (value) => {
             this.plugin.settings.skipWeekends = value;
             await this.plugin.saveSettings();
+          })
+      );
+
+    new Setting(containerEl)
+      .setName("Show icon in sidebar")
+      .setDesc("Show Tomorrow's Daily Note icon in the sidebar ribbon, allowing you to open tomorrow's daily note with a single click.")
+      .addToggle((toggle) =>
+        toggle
+          .setValue(this.plugin.settings.enableRibbonIcon)
+          .onChange(async (enableRibbonIcon) => {
+            this.plugin.settings.enableRibbonIcon = enableRibbonIcon;
+            await this.plugin.saveSettings();
+
+            if (enableRibbonIcon) {
+              this.plugin.ribbonHandler.addRibbonIcon();
+            } else {
+              this.plugin.ribbonHandler.removeRibbonIcon();
+            }
           })
       );
   }
